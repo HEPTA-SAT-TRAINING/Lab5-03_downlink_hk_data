73f3b977bb79690f66044713dbc447b8a1ef8d68
masa-night <csms21050@g.nihon-u.ac.jp>
2026-06-12 12:54:11 +0900

    Rename submodule path from HEPTA-SAT-Library to src

--- Lab5-03_downlink_hk_data.ino
+++ Lab5-03_downlink_hk_data.ino
@@ -15,11 +15,11 @@ void downlink_hk_data(void) {
   float ichg = eps.get_current_charge();
 
   com.printf(
-    "TEMP=%.2f,VBAT=%.3f,V5=%.3f,V3V3=%.3f,SAP=%.3f,IDIS=%.3f,ICHG=%.3f\n",
+    "TEMP=%.2f,VBAT=%.3f,V5=%.3f,V3V3=%.3f,SAP=%.3f,IDIS=%.3f,ICHG=%.3f\r\n",
     temp, vbat, v5, v3v3, sap, idis, ichg);
 
   cdh.printf(
-    "HK: TEMP=%.2f C, VBAT=%.3f V, V5=%.3f V, V3V3=%.3f V, SAP=%.3f V, IDIS=%.3f A, ICHG=%.3f A\n",
+    "HK: TEMP=%.2f C, VBAT=%.3f V, V5=%.3f V, V3V3=%.3f V, SAP=%.3f V, IDIS=%.3f A, ICHG=%.3f A\r\n",
     temp, vbat, v5, v3v3, sap, idis, ichg);
 }
 
@@ -39,8 +39,8 @@ void setup() {
 void loop() {
   if (com.available()) {
     char cmd = com.get_char();
-    com.printf("command = %c\n", cmd);
-    cdh.printf("command = %c\n", cmd);
+    com.printf("command = %c\r\n", cmd);
+    cdh.printf("command = %c\r\n", cmd);
   } else {
     downlink_hk_data();
   }
